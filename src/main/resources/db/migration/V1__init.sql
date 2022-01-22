01dad14e724387e505f18835b8bc4a996fb697b9
rxcats <windpyj80@gmail.com>
2022-01-22 21:02:01 +0900

    db schema collate to utf8mb4_0900_ai_ci

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v13.1.8 (64 bit)
-MySQL - 5.7.36-39 : Database - game_db
+MySQL - 8.0.27 : Database - game_db
 *********************************************************************
 */
 
@@ -12,7 +12,7 @@ MySQL - 5.7.36-39 : Database - game_db
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`game_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`game_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 USE `game_db`;
 
@@ -21,41 +21,41 @@ USE `game_db`;
 DROP TABLE IF EXISTS `tournament_group`;
 
 CREATE TABLE `tournament_group` (
-  `tid` bigint(20) NOT NULL DEFAULT '0',
-  `group_id` int(11) NOT NULL DEFAULT '0',
-  `user_count` int(11) NOT NULL DEFAULT '0',
-  `total_user_count` int(11) NOT NULL DEFAULT '0',
+  `tid` bigint NOT NULL DEFAULT '0',
+  `group_id` int NOT NULL DEFAULT '0',
+  `user_count` int NOT NULL DEFAULT '0',
+  `total_user_count` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`tid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 /*Table structure for table `tournament_group_member` */
 
 DROP TABLE IF EXISTS `tournament_group_member`;
 
 CREATE TABLE `tournament_group_member` (
-  `tid` bigint(20) NOT NULL DEFAULT '0',
-  `uid` bigint(20) NOT NULL DEFAULT '0',
-  `group_id` int(20) NOT NULL DEFAULT '0',
+  `tid` bigint NOT NULL DEFAULT '0',
+  `uid` bigint NOT NULL DEFAULT '0',
+  `group_id` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`tid`,`uid`,`group_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 /*Table structure for table `user` */
 
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
-  `uid` bigint(20) NOT NULL DEFAULT '0',
-  `platform_uid` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `platform_code` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `nickname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `user_pic` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `user_lv` int(11) NOT NULL DEFAULT '0',
-  `user_state` varchar(2) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `uid` bigint NOT NULL DEFAULT '0',
+  `platform_uid` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `platform_code` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `user_pic` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `user_lv` int NOT NULL DEFAULT '0',
+  `user_state` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
   `login_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`uid`),
   KEY `i_platform_uid` (`platform_uid`),
   KEY `i_nickname` (`nickname`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
